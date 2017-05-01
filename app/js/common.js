ce2144f16c49caa12e94d9f9119ff66dd9404a74
Dima Lenkov <bagjino@gmail.com>
2017-05-01 22:50:31 +0300

    first commit

---
+++ app/js/common.js
@@ -0,0 +1,40 @@
+$(function() {
+	
+  var Menu = {
+  
+  el: {
+    ham: $('.menu'),
+    menuTop: $('.menu-top'),
+    menuMiddle: $('.menu-middle'),
+    menuBottom: $('.menu-bottom'),
+	menuUL: $('.top-mnu')
+  },
+  
+  init: function() {
+    Menu.bindUIactions();
+  },
+  
+  bindUIactions: function() {
+    Menu.el.ham
+        .on(
+          'click',
+        function(event) {
+        Menu.activateMenu(event);
+        event.preventDefault();
+      }
+    );
+  },
+  
+  activateMenu: function() {
+    Menu.el.menuTop.toggleClass('menu-top-click');
+    Menu.el.menuMiddle.toggleClass('menu-middle-click');
+    Menu.el.menuBottom.toggleClass('menu-bottom-click'); 
+	Menu.el.menuUL.slideToggle();
+  }
+};
+
+Menu.init();
+
+
+
+});
